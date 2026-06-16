c4912642b8606f7d74c9da411a828a804c3210e1
Maréme Thiaw <mariamathiaw92@gmail.com>
2026-06-16 09:28:14 +0100

    Implémentation de l'authentification

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -3,7 +3,6 @@ import { CanActivateFn, Router } from '@angular/router';
 import { Auth } from '../services/auth';
 
 export const authGuard: CanActivateFn = () => {
-    console.log('Guard exécuté');
     const authService = inject(Auth);
     const router = inject(Router);
 
--- src/app/features/dashboard/agence-dashboard/agence-dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-agence-dashboard',
-  imports: [],
-  templateUrl: './agence-dashboard.html',
-  styleUrl: './agence-dashboard.css',
-})
-export class AgenceDashboard {}
